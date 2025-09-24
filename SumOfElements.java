0d295a8008d04cfb1f8da6ba2806e7a63d9dcf1f
Kaan Emre Öztürk <kaanemre2006@gmail.com>
2025-09-24 16:25:42 +0300

    Sum of elements

---
+++ SumOfElements.java
@@ -0,0 +1,25 @@
+
+
+public class SumOfElements {
+
+    public static int sumOfElements(int arr[], String str) {
+        int sum = 0;
+        if(str.equals("even")){
+            for(int i = 0; i < arr.length;i++){
+                if(i % 2 != 0){
+                    sum += arr[i];
+                }
+            }
+        }
+        else if(str.equals("odd")){
+            for(int i = 0; i < arr.length;i++){
+                if(i % 2 == 0){
+                    sum += arr[i];
+                }
+            }
+        }
+        return sum;
+
+        
+    }
+}
