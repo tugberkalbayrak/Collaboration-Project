8e81fd40724277a25a33c287b9f44ec21c0f566e
rendekar8 <abdullah.ozen@ug.bilkent.edu.tr>
2025-09-24 22:38:57 +0300

    Menu class has been added

--- SumOfElements.java
+++ SumOfElements.java
@@ -6,14 +6,14 @@ public class SumOfElements {
         int sum = 0;
         if(str.equals("even")){
             for(int i = 0; i < arr.length;i++){
-                if(i % 2 != 0){
+                if(i % 2 == 0){
                     sum += arr[i];
                 }
             }
         }
         else if(str.equals("odd")){
             for(int i = 0; i < arr.length;i++){
-                if(i % 2 == 0){
+                if(i % 2 != 0){
                     sum += arr[i];
                 }
             }
---
+++ menu.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        Scanner scn = new Scanner(System.in);
+        int arraySize;
+        int option = 0;
+        String oddOrEven;
+
+
+        System.out.print("Enter the size of the array: ");
+        arraySize = scn.nextInt();
+
+        int[] arr1 = GenerateArray.generateArray(arraySize);
+
+        while(option != 5){
+            System.out.printf("\nWith this random generated array with the size of %d, you want to: ", arraySize);
+            System.out.println(
+            "\n1) Calculate (element - average) " + 
+            "\n2) Find the maximum value in the array "+
+            "\n3) Find the minimum value in the array " +
+            "\n4) Calculate the sum of odd or even elements in the array" + 
+            "\n5) Or you may exit");
+            option = scn.nextInt();
+
+            if(option == 1) {
+                Average.findAverage(arr1);
+            }
+            else if(option == 2) {
+                System.out.println("\nThe maximum value in the array is: "+ Operations.max(arr1));
+            }
+            else if(option == 3) {
+                System.out.println("\nThe minimum value in the array is: "+ Operations.min(arr1));
+            }
+            else if(option == 4) {
+                System.out.print("\nWhether (odd) or (even) elements' sum you want to calculate? ");
+                oddOrEven = scn.next();
+                int sum = SumOfElements.sumOfElements(arr1, oddOrEven);
+                System.out.printf("\nSum of %s elements in the array is %d\n", oddOrEven, sum);
+            }
+        }
+
+
+
+
+
+    }
+}
